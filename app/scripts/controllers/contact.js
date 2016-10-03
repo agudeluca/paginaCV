398d4c7ccc8251529d774f24452e16c55905b1d0
agudeluca <agustin_deluca@hotmail.com>
2016-10-03 18:34:52 -0300

    sdafsdf

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,10 +8,22 @@
  * Controller of the agustindelucaApp
  */
 angular.module('agustindelucaApp')
-  .controller('ContactCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ContactCtrl', function ($scope, $http, $window) {
+     $scope.form = {};
+  $scope.processForm = function() {
+    $http({
+      method  : 'POST',
+      url     : 'http://agustindeluca.com/angularSender.php',
+      data    : $scope.form,
+      headers : { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })
+    .then(function(){
+  		$window.alert('Thank you for the feedback. We will get back to you if required');
+  		$scope.form = {};
+        console.log();
+    });
+
+      // console.log($scope.formData);
+  };
+   
   });
